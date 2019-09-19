27164c0602a7a2c1ecedcf40533af5c8d32ba2bd
Keyneloui <keyne.loui@gmail.com>
2019-09-19 12:04:57 +0530

    added

--- src/main/java/com/revature/util/ConnectionUtil.java
+++ src/main/java/com/revature/util/ConnectionUtil.java
@@ -11,54 +11,72 @@ import java.util.Properties;
 import java.util.Scanner;
 
 public class ConnectionUtil {
-	 private static final ConnectionUtil INSTANCE = new ConnectionUtil();
-	    
-	    private ConnectionUtil() {
-	        //private Constructor
-	    }
-	    
-	    public static ConnectionUtil getInstance() {
-	        return INSTANCE;
-	    }
-	 
+	private static final ConnectionUtil INSTANCE = new ConnectionUtil();
+
+	private ConnectionUtil() {
+		// private Constructor
+	}
+
+	public static ConnectionUtil getInstance() {
+		return INSTANCE;
+	}
+
+	/*
+	 * public static Connection getConnection() {
+	 * 
+	 * 
+	 * Connection con = null; //try with resources, it will the close the
+	 * reader.close automatically. try(InputStream inputStream = INSTANCE.getClass()
+	 * .getClassLoader().getResourceAsStream("application.properties")) {
+	 * 
+	 * Properties prop = new Properties(); // load a properties file
+	 * prop.load(inputStream);
+	 * 
+	 * String driverClassName = prop.getProperty("driverClassName"); String url =
+	 * prop.getProperty("url"); String username = prop.getProperty("username");
+	 * String password = prop.getProperty("password");
+	 * Class.forName(driverClassName); con = DriverManager.getConnection(url,
+	 * username, password);
+	 * System.out.println("Loading Credentials from Property Files:" + con); } catch
+	 * (ClassNotFoundException e) { e.printStackTrace(); throw new
+	 * RuntimeException("Unable to load the driver class"); } catch (SQLException e)
+	 * { e.printStackTrace(); throw new
+	 * RuntimeException("Unable to get DB Connection"); } catch (IOException ex) {
+	 * ex.printStackTrace(); throw new
+	 * RuntimeException("Unable to read property files"); }
+	 * 
+	 * 
+	 * 
+	 * return con;
+	 * 
+	 * }
+	 */
 
 	public static Connection getConnection() {
-
+		String driverClassName = "com.mysql.cj.jdbc.Driver";
+		String url = "jdbc:mysql://localhost:3306/Charity_app";
+		String username = "root";
+		String password = "root";
 
 		Connection con = null;
-		 //try with resources, it will the close the reader.close automatically.
-        try(InputStream inputStream  = INSTANCE.getClass()
-                .getClassLoader().getResourceAsStream("application.properties")) {
-            
-            Properties prop = new Properties();
-            // load a properties file
-            prop.load(inputStream);
 
-            String driverClassName = prop.getProperty("driverClassName");
-            String url = prop.getProperty("url");
-            String username = prop.getProperty("username");
-            String password = prop.getProperty("password");
-            Class.forName(driverClassName);
-            con = DriverManager.getConnection(url, username, password);
-            System.out.println("Loading Credentials from Property Files:" + con);
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-            throw new RuntimeException("Unable to load the driver class");
-        } catch (SQLException e) {
-            e.printStackTrace();
-            throw new RuntimeException("Unable to get DB Connection");
-        } catch (IOException ex) {
-            ex.printStackTrace();
-            throw new RuntimeException("Unable to read property files");
-        }
-
-
+		try {
+			Class.forName(driverClassName);
+			con = DriverManager.getConnection(url, username, password);
+			// System.out.println(con);
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+			throw new RuntimeException("Unable to load the driver class");
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw new RuntimeException("Unable to get DB Connection");
+		}
 
 		return con;
 
 	}
-	public static void main(String[] args)
-	{
+
+	public static void main(String[] args) {
 		getConnection();
 	}
 
